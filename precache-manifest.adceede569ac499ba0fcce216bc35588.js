7490217023f65e00f2b130a27422e0a1f739f4a4
Shayan Anoushiravani <shayananoushiravani@Shayans-MacBook-Pro.local>
2019-04-26 11:13:21 -0700

    Updates

---
+++ precache-manifest.adceede569ac499ba0fcce216bc35588.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2326e341011857863847e7bb8f8f376b",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "362d802769b40aefd2ae",
+    "url": "/clicky-game/static/css/main.ccd4b64f.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "362d802769b40aefd2ae",
+    "url": "/clicky-game/static/js/main.4a5f05bb.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
